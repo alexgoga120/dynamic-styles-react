4c984fafbd5b68fccb0c27d3552b039b51d8ce5f
alex <alex.satoritech@gmail.com>
2023-02-07 11:40:17 -0600

    remove styled-components usages

--- src/components/CourseGoals/CourseInput/CourseInput.tsx
+++ src/components/CourseGoals/CourseInput/CourseInput.tsx
@@ -1,15 +1,14 @@
-// import './CourseInput.css';
+import styles from './CourseInput.module.css';
 
 import {ChangeEvent, FormEvent, useState} from 'react';
 
 import Button from '../../UI/Button/Button';
 import PropsCourseInput from './PropsCourseInput';
-import styled from "styled-components";
+// import styled from "styled-components";
 
-
-type PropsStyled =  {
-    invalid:boolean
-}
+// type PropsStyled = {
+//     invalid: boolean
+// }
 
 const CourseInput = (props: PropsCourseInput) => {
     const [enteredValue, setEnteredValue] = useState<string>('');
@@ -31,42 +30,42 @@ const CourseInput = (props: PropsCourseInput) => {
         props.onAddGoal(enteredValue);
     };
 
-    const FormControl = styled.div<PropsStyled>`
-      & {
-        margin: 0.5rem 0;
-      }
-
-      & label {
-        font-weight: bold;
-        display: block;
-        margin-bottom: 0.5rem;
-        color: ${props => props.invalid ? 'red' : 'black'}
-      }
-
-      & input {
-        display: block;
-        width: 100%;
-        border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
-        background: ${props => props.invalid ? '#ffd7d7' : 'transparent'};
-        font: inherit;
-        line-height: 1.5rem;
-        padding: 0 0.25rem;
-      }
-
-      & input:focus {
-        outline: none;
-        background: #fad0ec;
-        border-color: #8b005d;
-      }
-    `
+    // const FormControl = styled.div<PropsStyled>`
+    //   & {
+    //     margin: 0.5rem 0;
+    //   }
+    //
+    //   & label {
+    //     font-weight: bold;
+    //     display: block;
+    //     margin-bottom: 0.5rem;
+    //     color: ${props => props.invalid ? 'red' : 'black'}
+    //   }
+    //
+    //   & input {
+    //     display: block;
+    //     width: 100%;
+    //     border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
+    //     background: ${props => props.invalid ? '#ffd7d7' : 'transparent'};
+    //     font: inherit;
+    //     line-height: 1.5rem;
+    //     padding: 0 0.25rem;
+    //   }
+    //
+    //   & input:focus {
+    //     outline: none;
+    //     background: #fad0ec;
+    //     border-color: #8b005d;
+    //   }
+    // `
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <FormControl invalid={hasErrors}>
+            <div className={`${styles['form-control']} ${hasErrors && styles.invalid}`}>
                 <label>Course Goal</label>
                 <input type="text"
                        onChange={goalInputChangeHandler}/>
-            </FormControl>
+            </div>
             <Button type="submit">Add Goal</Button>
         </form>
     );
